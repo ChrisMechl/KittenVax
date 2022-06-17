e40cdaafda45e070a21789ffd25ce4c962d437c1
Chris <christianmechler1@gmail.com>
2022-06-17 13:11:20 -0600

    Initial commit

---
+++ src/test/java/com/kit/KittenVax/KittenVaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.KittenVax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittenVaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
