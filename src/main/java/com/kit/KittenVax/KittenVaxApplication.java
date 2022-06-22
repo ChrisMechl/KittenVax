5920b8339c14c758dd5c4ac6e33d5ab8c2bab1f2
Chris <christianmechler1@gmail.com>
2022-06-22 17:38:21 -0600

    Added system shutdown option

--- src/main/java/com/kit/KittenVax/KittenVaxApplication.java
+++ src/main/java/com/kit/KittenVax/KittenVaxApplication.java
@@ -1,5 +1,7 @@
 package com.kit.KittenVax;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,14 +19,34 @@ public class KittenVaxApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KittenVaxApplication.class, args);
 		
-		RestClient client = new RestClient();
-//		client.purge(dbPath);
+		manageServer(true);
 		start();
+	}
+	
+	/* Executes server at port 3000. Presumably you'll need to change the location of the script and change the location of the database within the script */
+	public static void manageServer(boolean purge) {
+		RestClient client = new RestClient();
+		try {
+			Runtime.getRuntime().exec("/home/chris/workspace/KittenVax/startServer.sh");
+		} 
+		catch (IOException e) {
+			System.err.println("Failed to run server start script");
+			return;
+		}
+		if(purge) client.purge(dbPath);
 	}
 	
 	private static void start() {
 		ActorSystem<Vet.Command> mySystem = ActorSystem.create(Vet.create(), "mySystem");
 		mySystem.tell(new Vet.Start(5, 5, mySystem));
+
+		try {
+		      System.out.println(">>> Press ENTER to exit <<<");
+		      System.in.read();
+		    } catch (IOException ignored) {
+		    } finally {
+		      mySystem.terminate();
+		    }
 	}
 
 }
