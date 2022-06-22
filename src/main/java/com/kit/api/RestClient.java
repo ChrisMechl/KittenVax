5f0c42f17c676b16bdcdd08e98b6d6173f948e11
Chris <christianmechler1@gmail.com>
2022-06-22 14:20:44 -0600

    Added methods to GET individual kittens, GET all, POST individuals, and purge the db to remove all elements

--- src/main/java/com/kit/api/RestClient.java
+++ src/main/java/com/kit/api/RestClient.java
@@ -1,23 +1,175 @@
 package com.kit.api;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.google.gson.Gson;
+import com.kit.KittenVax.Kitten;
 
 public class RestClient {
-	private static final String URI = "http://localhost:3000";
+	private static final String URI = "http://localhost:3000/kittens";
 	
-	private Client client = ClientBuilder.newClient();
+	private HttpClient client = HttpClientBuilder.create().build();
+	private static Gson gson = new Gson();	
 	
-	WebTarget target = client.target(URI);
+	/* Resets the json file at the path location. It creates a single array with a "kittens" tag */
+	public boolean purge(String path) {
+		BufferedWriter db;
+		try {
+			db = new BufferedWriter(new FileWriter(path));
+		}
+		catch(IOException e) {
+			System.err.println("Failed to open file at " + path);
+			System.err.println("Make sure server isn't running");
+			return false;
+		}
+		
+		String defaultDB = "{\"kittens\":[]}";
+		
+		try {
+			db.write(defaultDB);
+		} catch (IOException e) {
+			System.err.println("Failed to write to file at " + path);
+			System.err.println("Make sure server isn't running");
+			return false;
+		}
+		finally {
+			try {
+				db.close();
+			} catch (IOException e) {
+				System.err.println("Failed to close writer");
+			}
+		}
+		
+		return true;
+	}
 	
+	/* Attempts to post the given kitten to the server */
+	public boolean postRequest(Kitten k) {
+		HttpPost post = new HttpPost(URI);
+		StringEntity toPersist;
+		
+		try {	
+			toPersist = new StringEntity(gson.toJson(k));
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("Failed creating Json representation of Kitten: " + k);
+			return false;			
+		}
+		
+		post.setEntity(toPersist);
+		post.setHeader("Content-type", "application/json");
+		
+		HttpResponse resp;
+		try {
+			resp = client.execute(post);
+		}
+		catch (ClientProtocolException e) {
+			System.err.println("Error in HTTP protocol for POST with Kitten: " + k);
+			return false;
+		}
+		catch (IOException e) {
+			System.err.println("Failure to POST due to IO Exception");
+			return false;
+		}
+		
+		if(resp.getStatusLine().getStatusCode() == 201) return true;
+		System.err.println("Problem posting to server. Status code: " + resp.getStatusLine().getStatusCode());
+		return false;
+	}
 	
-	public String getRequest() {
-		Response response = target.path("books").queryParam("id", "1", "The Tale of Two Cities").request().get();
-//		Response response = target.path("books").queryParam("id", "1", "The Tale of Two Cities").request().accept(MediaType.APPLICATION_JSON).get();
-		System.out.println(response.getStatus());
-		return response.readEntity(String.class);
+	/* Prints kittens by name */
+	public boolean getByName(Kitten k) {
+		URIBuilder builder;
+		HttpResponse resp;
+		HttpGet get;
+		InputStream in;
+		
+		try {
+			builder = new URIBuilder(URI);
+			get = new HttpGet(builder.build());
+			builder.setParameter("name", k.getName());
+		} 
+		catch (URISyntaxException e1) {
+			System.err.println("Failed to create GET URI due to bad syntax");
+			return false;
+		}
+		
+		try {
+			resp = client.execute(get);
+			in = resp.getEntity().getContent();
+		} 
+		catch (ClientProtocolException e) {
+			System.err.println("Error in HTTP protocol for GET with Kitten " + k);
+			return false;
+		} 
+		catch (IOException e) {
+			System.err.println("Failure to GET due to IO Exception");
+			return false;
+		}
+		
+		String text = new BufferedReader(
+			      new InputStreamReader(in, StandardCharsets.UTF_8))
+			        .lines()
+			        .filter(c -> !c.equals("[") && !c.equals("]"))
+			        .collect(Collectors.joining("\n"));
+		
+		System.out.println(text);
+		return true;
+	}
+	
+	/* Gets all Kittens and prints them to console */
+	public boolean getAll() {
+		URIBuilder builder;
+		HttpResponse resp;
+		HttpGet get;
+		InputStream in;
+		
+		try {
+			builder = new URIBuilder(URI);
+			get = new HttpGet(builder.build());
+		} 
+		catch (URISyntaxException e1) {
+			System.err.println("Failed to create GET URI due to bad syntax");
+			return false;
+		}
+		
+		try {
+			resp = client.execute(get);
+			in = resp.getEntity().getContent();
+		} 
+		catch (ClientProtocolException e) {
+			System.err.println("Error in HTTP protocol for GET all Kittens");
+			return false;
+		} 
+		catch (IOException e) {
+			System.err.println("Failure to GET due to IO Exception");
+			return false;
+		}
+		
+		String text = new BufferedReader(
+			      new InputStreamReader(in, StandardCharsets.UTF_8))
+			        .lines()
+			        .filter(c -> !c.equals("[") && !c.equals("]"))
+			        .collect(Collectors.joining("\n"));
+		
+		System.out.println(text);
+		return true;
 	}
 }
